c18b0579057a4d34f0e42e918234d2e22c1d5aae
Facite <89388117+facitea@users.noreply.github.com>
2021-10-12 03:22:24 +0900

    node.js를 통한 서버 구축 중

---
+++ main_pro.js
@@ -0,0 +1,72 @@
+var http = require('http'); // HTTP 모듈 : 웹 서버를 구동하기 위한 기본 모듈
+var fs = require('fs'); //filesystem : fs 모듈 사용 시 파일 시스템에 접근할 수 있다.
+var url = require('url'); // url 모듈 : 사이트 주소 정보를 url 객체로 만들 수 있다.
+ 
+function templateHTML(title, list, body){
+  return `
+  <!doctype html>
+  <html>
+  <head>
+    <title>WEB1 - ${title}</title>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <h1><a href="/">WEB</a></h1>
+    ${list}
+    ${body}
+  </body>
+  </html>
+  `;
+}
+function templateList(filelist){
+  var list = '<ul>';
+  var i = 0;
+  while(i < filelist.length){
+    list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+    i = i + 1;
+  }
+  list = list+'</ul>';
+  return list;
+}
+ 
+var app = http.createServer(function(request,response){
+    var _url = request.url;
+    var queryData = url.parse(_url, true).query;
+    var pathname = url.parse(_url, true).pathname;
+    if(pathname === '/'){
+      if(queryData.id === undefined){
+        fs.readdir('./data', function(error, filelist){
+          var title = 'Welcome';
+          var description = 'Hello, Node.js';
+          var list = templateList(filelist);
+          var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
+          response.writeHead(200);
+          response.end(template);
+        })
+      } else {
+        fs.readdir('./data', function(error, filelist){
+          fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
+            var title = queryData.id;
+            var list = templateList(filelist);
+            var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
+            response.writeHead(200);
+            response.end(template);
+          });
+        });
+      }
+    } else {
+      response.writeHead(404);
+      response.end('Not found');
+    }
+ 
+ 
+ 
+});
+app.listen(8080);
+    
+//localhost:8080/index.html
+
+//localhost:3000?id=HTML 같은 식으로 id 값을 주어야 읽을 수 있음
+
+//query string이란 ?id=HTML&page=12 같은 것.
+//물음표로 시작하기로 약속함
